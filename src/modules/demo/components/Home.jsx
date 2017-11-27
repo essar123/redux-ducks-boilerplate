f23dbad3fb757ea2959bbb28bfa15686682fc277
sushil kumar rathore <sushil.rathore@practo.com>
2017-11-27 22:24:30 +0530

    added support for css

--- src/modules/demo/components/Home.jsx
+++ src/modules/demo/components/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import style from '../../../App.css';
+import SubComp from './SubComp';
 import PropTypes from 'prop-types';
 
 const Home = (props) => {
   return (
-    <div>
+    <div className="home">
       <h1>{`Welcome to ${props.name}`}</h1>
+      <SubComp/>
     </div>
   )
 };
---
+++ src/modules/demo/components/SubComp.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SubComp = () => {
+  return (
+    <div className="home-sub">
+      SubComp
+    </div>
+  )
+}
+
+export default SubComp;
